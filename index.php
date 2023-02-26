42110a801f5f49e2525da61511f575a06c8e5fcc
klericss <nikitashishkov192@gmail.com>
2023-02-26 16:42:40 +0300

    Website for a company K-Town
    Website for a company K-Town

---
+++ index.php
@@ -0,0 +1,322 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>K-town</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" type="text/css" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+  </head>
+  <body>
+    <!-- HEADER START -->
+    <header class="header">
+      <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container">
+          <a class="navbar-brand" href="#"><img src="img/logo.png" alt="logotip" width="290"></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link fs-6" aria-current="page" href="#about">О нас</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link fs-6" href="#contacts">Наши контакты</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link nav-link fs-6" href="#delivery">Доставка и оплата</a>
+              </li>
+            </ul>
+            <span class="navbar-text">
+                <div>Свяжитесь с нами: +135 456 35 72</div>
+                <!-- <div><i class="fa-solid fa-location-dot"></i> Москва, метро Дубровка, ул Новоостаповская 6ас3</div> -->
+            </span>
+          </div>
+        </div>
+      </nav>
+    </header>
+    <!-- HEADER END -->
+
+
+
+    <!-- MAIN START -->
+    <main class="content">
+      <section class="container my-5 first_slider">
+        <div id="carouselExampleDark" class="carousel carousel-dark slide">
+          <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          </div>
+          <div class="carousel-inner">
+            <div class="carousel-item active" data-bs-interval="10000">
+              <img src="img/banner1.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item" data-bs-interval="2000">
+              <img src="img/banner5.jpg" class="d-block w-100" alt="...">
+            </div>
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
+      </section>
+      
+      <!-- Section slider2 -->
+      <!-- <section class="container my-5 bg_slider">
+        <h3 class="text-center title_brend">Популярные Бренды</h3>
+          <div class="owl-carousel owl-theme">
+            <img style="width: 120px" src="img/adio-footwear.png">
+            <img style="width: 120px" src="img/osdiris-carpusel.png">
+            <img style="width: 120px" src="img/reflex_carousel.png">
+            <img style="width: 120px" src="img/dws_carousel.png">
+            <img style="width: 120px" src="img/vans_carousel.png">
+            <img style="width: 120px" src="img/circa_carousel.png">
+          </div>
+      </section> -->
+      <!-- Section slider2 end-->
+
+      <!-- Section popular -->
+      <section class="container card-main mt-5 my-5">
+          <h3 class="mb-5 text-center">Популярные товары в нашем магазине</h3>
+          <div class="mb-5" style="height: 2px; background-color: #852cf5; width: 300px; margin: 0 auto;"></div>
+          <div class="row g-0 text-center">
+            <div class="col-md-3" data-aos="flip-left">
+              <div class="card border-0 first_card">
+              <img src="img/card1.png" class="card-img-top" alt="...">
+              <div class="card-body">
+                <p class="card-title">Кроссовки ADIO</p>
+              </div>
+              </div>
+            </div>
+            <div class="col-md-3" data-aos="flip-left">
+              <div class="card border-0">
+              <img src="img/card2.png" class="card-img-top" alt="...">
+              <div class="card-body">
+                <p class="card-title">Кроссовки CIRCA</p>
+              </div>
+              </div>
+            </div>
+            <div class="col-md-3" data-aos="flip-left">
+              <div class="card border-0">
+              <img src="img/card3.png" class="card-img-top" alt="...">
+              <div class="card-body">
+                <p class="card-title">Кроссовки CIRCA</p>
+              </div>
+              </div>
+            </div>
+            <div class="col-md-3" data-aos="flip-left">
+              <div class="card border-0">
+              <img src="img/card4.png" class="card-img-top" alt="...">
+              <div class="card-body">
+                <p class="card-title">Кроссовки REFLEX</p>
+              </div>
+              </div>
+            </div>
+            <div class="col-md-3 mt-5" data-aos="flip-left">
+              <div class="card border-0">
+              <img src="img/card5.png" class="card-img-top" alt="...">
+              <div class="card-body">
+                <p class="card-title">Кроссовки OSIRIS D3</p>
+              </div>
+              </div>
+            </div>
+            <div class="col-md-3 mt-5" data-aos="flip-left">
+              <div class="card border-0">
+              <img src="img/card6.png" class="card-img-top" alt="...">
+              <div class="card-body">
+                <p class="card-title">Кроссовки CIRCA</p>
+              </div>
+              </div>
+            </div>
+            <div class="col-md-3 mt-5" data-aos="flip-left">
+              <div class="card border-0">
+              <img src="img/card7.png" class="card-img-top" alt="...">
+              <div class="card-body">
+                <p class="card-title">Кроссовки DWS</p>
+              </div>
+              </div>
+            </div>
+            <div class="col-md-3 mt-5" data-aos="flip-left">
+              <div class="card border-0">
+              <img src="img/card8.png" class="card-img-top" alt="...">
+              <div class="card-body">
+                <p class="card-title">Кроссовки CUSTOM</p>
+              </div>
+              </div>
+            </div>
+          </div>
+      </section>
+      <!-- Section popular end -->
+
+      <!-- Section banner -->
+      <section class="container my-5 d-flex justify-content-center" data-aos="fade-down">
+        <div class="row">
+          <div class="col-12">
+            <img class="img-fluid mt-5 mb-5" src="img/banner3.jpg" alt="banner3">
+          </div>
+        </div>
+      </section>
+      <!-- Section banner end -->
+
+      <!-- Section preimushestv -->
+      <section id="delivery" class="container my-5">
+        <div class="d-flex flex-column justify-content-center align-items-center">
+          <h1 class="text-center mb-5">Почему именно мы?</h1>
+          <div style="height: 2px; background-color: #852cf5; width: 400px; margin: 0 auto; margin-bottom: 30px;"></div>
+          <p class="text-center mb-5 fs-5 w-50">Мы упорно работаем каждый день, чтобы сделать жизнь наших клиентов лучше и счастливее</p>
+        </div>
+        <div class="ars-bot-servies text-center">
+          <div class="row d-flex justify-content-center align-items-center">
+            <div class="col-4" data-aos="fade-down">
+              <img class="mb-3" src="img/dostavka.png" alt="hp-delivery_icon" width="70">
+              <h4>Доставка/оплата</h4>
+              <p class="light-color mt-3">Оплата наличными курьеру или банковской картой без комиссии</p>
+            </div>
+            <div class="advantages col-4" data-aos="fade-down">
+              <img class="mb-3" src="img/vozvrat.png" alt="hp-delivery_icon" width="70">
+              <h4>Условия возврата</h4>
+              <p class="light-color mt-3">Быстрый возврат, если товар не соответствует качеству</p>
+            </div>
+            <div class="col-4" data-aos="fade-down">
+              <img class="mb-3" src="img/Garantiya.png" alt="hp-delivery_icon" width="70">
+              <h4>Гарантийное обслуживание</h4>
+              <p class="light-color mt-3">Оплата наличными курьеру или банковской картой без комиссии</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- Section preimushestv end-->
+
+
+
+      <!-- About start -->
+      <section id="delivery" class="container my-5">
+        <div class="d-flex flex-column justify-content-center align-items-center">
+          <h1 class="text-center mb-5">О нас</h1>
+          <div class="mb-5" style="height: 2px; background-color: #852cf5; width: 300px; margin: 0 auto;"></div>
+          <p class="text-center mb-5 fs-5 w-50">K-town — это компания по продаже эксклюзивных кед и джинс, а также других вещей из 2000-х годов. Мы дает возможность окунуться в эпоху прошлых времен с чего все начиналось: эмо, панки, рэпаки - это одни из течений субкультур, благодаря которым зародились многие нынешние движения. Предлагаем познакомиться с ними поближе - в наших товарах много интересного!</p>
+        </div>
+      </section>
+      <!-- About end -->
+
+      <!-- Section contaks -->
+      <section id="contacts" class="container my-5" data-aos="fade-down">
+        <div class="text-center">
+          <h1 class="mb-5">Наши контакты</h1>
+          <div style="height: 2px; background-color: #852cf5; width: 300px; margin: 0 auto;"></div>
+          <p class="fs-5 mt-5 mb-5"><i class="me-3 fa-solid fa-location-dot"></i>Москва, метро Дубровка, ул Новоостаповская 6ас3</p>
+          <p class="mt-3"><i class="me-3 fa-solid fa-phone"></i>Свяжитесь с нами: +135 456 35 72</p>
+          <p><i class="me-3 mb-5 fa-solid fa-envelope"></i>Наша почта: hello@loftpineapple.com</p>
+        </div>
+        <div class="map d-flex align-items-center justify-content-center">
+          <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Ad73f470c9e1952215083ef472c2565d0159a82925dcc59231d027f1c62182a32&amp;source=constructor" width="874" height="450" frameborder="0"></iframe>
+        </div>
+      </section>
+      <!-- Section contaks end -->
+
+      <!-- Section forms -->
+      <section class="container my-5 p-3 mb-5">
+        <div class="about_form mt-3 d-flex flex-column align-items-center">
+          <h1 class="text-center mt-5 mb-5">Свяжитесь с нами по любым вопросам</h1>
+          <div style="height: 2px; background-color: #852cf5; width: 700px; margin: 0 auto; margin-bottom: 30px;"></div>
+          <p class="text-center fs-5 w-50">Мы всегда рады коллаборациям, интересным проектам и отзывам наших клиентов</p>
+        </div>
+        <div class="first_form mt-5">
+          <form class="row g-3 needs-validation d-flex flex-column align-items-center" novalidate>
+            <div class="col-md-5">
+              <input type="text" class="form-control p-3" id="validationCustom01" placeholder="Ваше имя" required>
+            </div>
+            <div class="col-md-5">
+              <input type="text" class="form-control p-3" id="validationCustom02" placeholder="Номер телефона" required>
+            </div>
+            <div class="col-md-5">
+            <textarea class="form-control" id="validationTextarea" placeholder="Введите сообщение" required></textarea>
+            </div>
+            <div class="col-md-3 d-flex flex-column align-items-center">
+              <button class="btn btn-dark p-3 w-50" type="submit">Отправить</button>
+            </div>
+          </form>
+          <div class="about_formss text-center mt-3 d-flex flex-column align-items-center">
+            <p class="w-50 light-color d-flex align-items-center">Нажимая на кнопку, вы даете согласие на обработку персональных данных и соглашаетесь c политикой конфиденциальности</p>
+          </div>
+        </div>
+      </section>
+      <!-- Section forms end-->
+    </main>
+    <!-- MAIN END -->
+
+
+
+
+    <!-- FOOTER START -->
+    <!-- Footer -->
+<footer class="text-center text-lg-start text-muted">
+  <!-- Section: Links  -->
+  <section class="footer_desc">
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <!-- Content -->
+          <a class="navbar-brand" href="#"><img class="footer_img" src="img/logotip.png" alt="logotip" width="70"></a>
+          <div class="first_name">
+            <h4 class="mx-2 mt-2">K-town</h4>
+            <p class="mx-2">Магазин эксклюзивной обуви</p>
+            <p><i class="fa-solid fa-calendar-days me-3 text-secondary mx-2"></i>Сб-Вс 15:00-21:00</p>
+          </div>
+        </div>
+        <!-- Grid column -->
+
+         <!-- Grid column -->
+         <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4 p-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">Подпишись</h6>
+          <p>
+            <img class="icon" src="img/instagram.svg" alt="instagram">
+            <img class="icon" src="img/Vkontakte.svg" alt="instagram" width="40">
+          </p> 
+          <p><i class="fas fa-envelope me-3 text-secondary"></i>ktown12@gmail.com</p>
+          <p><i class="fas fa-phone me-3 text-secondary"></i>+7(345) 355-64-43</p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4 p-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">Наш адресс</h6>
+          <p><i class="fas fa-home me-3 text-secondary"></i>Москва, метро Дубровка, ул Новоостаповская 6ас3</p>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+  </section>
+  <!-- Section: Links  -->
+  <hr>
+  <!-- Copyright -->
+  <div class="text-center" style="background-color: rgba(0, 0, 0, 0.025);">
+    © 2022-2023 Интернет-магазин "K-town"
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
+    <!-- FOOTER END -->
+    <script src="https://kit.fontawesome.com/f57b817149.js" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>AOS.init();</script>
+    <!-- <script src="./js/jquery.js"></script>
+    <script src="./js/owl.carousel.min.js"></script> -->
+    <script src="./js/script.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+  </body>
+</html>
